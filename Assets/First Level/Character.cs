c704e0949dada9bac04bb3cf8433c11ffefcc7fb
PHM\dchow62 <drasonchow@gmail.com>
2017-03-03 10:35:38 -0500

    xgdxhgfdfg

--- Assets/First Level/Character.cs
+++ Assets/First Level/Character.cs
@@ -1,47 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class character
 {
-                 
-    public Rigidbody2D rigidbody;
-    public Transform transform;
-    public Animator animator;
-    public SpriteRenderer spr_renderer;
-    public Collider2D col_Circle;
-    public Collider2D col_Box;
-    public Collider2D col_Polygon;
+    
+    public new Rigidbody2D rigidbody;
+    public new Transform transform;
+    public new Animator animator;
+    public new SpriteRenderer spr_renderer;
+    public new Collider2D col_Circle;
+    public new Collider2D col_Box;
+    public new Collider2D col_Polygon;
     public int direction;
     public float power_Jump;
     public float power_Speed;
     public float mass;
     public float altitude;
     public bool grounded;
-    
 }
-
-
-public class test_Character
-{
-
-    public Transform transform;
-    public BoxCollider2D col_Box;
-    public Animator animator;
-
-
-
-    
-   
-    
-}
-
 public class overworld
 {
     public float Fly_Power;
-    public Transform flyform;
-    public Animator flymator;
-    public Collider2D Fly_Circle;
-
-    
+    public new Transform flyform;
+    public new Animator flymator;
+    public new Collider2D Fly_Circle;
 }
