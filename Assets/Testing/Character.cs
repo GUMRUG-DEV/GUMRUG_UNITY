fab92bedbce047f0f354c432866136e73ce8e5ec
PHM\dchow62 <drasonchow@gmail.com>
2017-03-03 11:07:56 -0500

    Fixed Corrupted Code

---
+++ Assets/Testing/Character.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character
+{
+    
+    public Rigidbody2D rigidbody;
+    public Transform transform;
+    public Animator animator;
+    public SpriteRenderer spr_renderer;
+    public Collider2D col_Circle;
+    public Collider2D col_Box;
+    public Collider2D col_Polygon;
+    public int direction;
+    public float power_Jump;
+    public float power_Speed;
+    public float mass;
+    public float altitude;
+    public bool grounded;
+}
+public class overworld
+{
+    public float Fly_Power;
+    public Transform flyform;
+    public Animator flymator;
+    public Collider2D Fly_Circle;
+}
