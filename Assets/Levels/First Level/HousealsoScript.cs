592e67af6520710f93ded289a35b8ceaa5fece35
esmith25 <esmith25@phmschools.org>
2017-03-30 11:07:56 -0400

    jjj
    jjj

---
+++ Assets/Levels/First Level/HousealsoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HousealsoScript : MonoBehaviour {
+    public overworld Door = new overworld();
+    public overworld Hero = new overworld();
+    // Use this for initialization
+    void Start () {
+        Hero.col_Box = GameObject.Find("Gum").GetComponent<BoxCollider2D>();
+        Door.col_Box = GameObject.Find("DoorInside").GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Hero.col_Box.bounds.Intersects(Door.col_Box.bounds))
+        {
+            SceneManager.LoadScene("FirstLevel - OutsideF");
+        }
+    }
+}
