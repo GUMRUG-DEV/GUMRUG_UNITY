768659edf58f3c06768be6d1c903cabaa9076250
Jonathan Durr <johnny.durr357@gmail.com>
2017-06-01 11:20:47 -0400

    Item Drop and Drag

---
+++ Assets/Health and Combat/CursorSensor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if	(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left mouse button pressed");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right mouse button pressed");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("middle mouse button pressed");
+        }
+        Vector2 mousePos = Input.mousePosition;
+        Debug.Log(mousePos.x);
+        Debug.Log(mousePos.y);
+    }
+}
